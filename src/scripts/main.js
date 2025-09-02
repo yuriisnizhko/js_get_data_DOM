569684442d8d982032a46f4ed9d4e01adb689740
Yurii Snizhko <yuriisnizhko@gmail.com>
2025-09-02 20:55:53 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
 'use strict';
 
 // write your code here
+const spnPopulation = document.querySelectorAll('span.population');
+const total = document.querySelector('.total-population');
+const avarage = document.querySelector('.average-population');
+let sumAll = 0;
+
+for (let i = 0; i < spnPopulation.length; i++) {
+  sumAll += parseInt(spnPopulation[i].textContent.replaceAll(',', ''));
+}
+
+total.textContent = withComma(sumAll);
+avarage.textContent = withComma(Math.floor(sumAll / spnPopulation.length));
+
+function withComma(num) {
+  const result = num.toLocaleString('en-US');
+
+  return result;
+}
